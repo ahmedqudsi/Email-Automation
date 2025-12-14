bc03528126ef6b4f92805abea232c58ae5df6070
Ahmed Qudsi Ghouse Ali Khan <ahmedqudsi786@gmail.com>
2025-12-14 17:11:29 +0530

    updated config for email

--- app.py
+++ app.py
@@ -1,78 +1,75 @@
-# import smtplib
-# from email.mime.text import MIMEText
-# from email.mime.multipart import MIMEMultipart
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-# sender_email = "mohd.madani.02@gmail.com"
-# recipient_email = "ahmedqudsi786@gmail.com"
-# password = "wvla enyv kqox fmfs"
+sender_email = ""
+recipient_email = ""
+password = ""
 
-# subject = "Automated Email from Python"
-# body = "Hello! This is an automated email sent using Python."
-# msg = MIMEMultipart()
-# msg['From'] = sender_email
-# msg['To'] = recipient_email 
-# msg['Subject'] = subject
-# msg.attach(MIMEText(body, 'plain'))
+subject = "Automated Email from Python"
+body = "Hello! This is an automated email sent using Python."
+msg = MIMEMultipart()
+msg['From'] = sender_email
+msg['To'] = recipient_email 
+msg['Subject'] = subject
+msg.attach(MIMEText(body, 'plain'))
 
-# try:
-#     with smtplib.SMTP('smtp.gmail.com', 587) as server:
-#         server.starttls()
-#         server.login(sender_email, password)
-#         server.send_message(msg)
-#     print("Email sent successfully!")
-# except Exception as e:
-#     print(f"Error: {e}")
-
-
+try:
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(sender_email, password)
+        server.send_message(msg)
+    print("Email sent successfully!")
+except Exception as e:
+    print(f"Error: {e}")
 
 
 
-# import smtplib
-# import time
-# from email.mime.text import MIMEText
-# from email.mime.multipart import MIMEMultipart
-
-# sender_email = "mohd.madani.02@gmail.com"
-# password = "wvla enyv kqox fmfs"
-
-# recipients = [
-#     "ahmedqudsi786@gmail.com",
-#     "mr.khans00966@gmail.com",
-#     "qadriasma3@gmail.com",
-#     "mohd.madani.02@gmail.com"
-# ]
-
-# subject = "Application for Potential Opportunities"
-
-# body = """Dear Hiring Team,
-
-# I hope this message finds you well. I am reaching out to explore potential opportunities.
-# Please find my resume attached for your consideration.
-
-# Warm regards,
-# Ahmed
-# """
 
 
-# with smtplib.SMTP("smtp.gmail.com", 587) as server:
-#     server.ehlo()
-#     server.starttls()
-#     server.login(sender_email, password)
+import smtplib
+import time
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-#     for email in recipients:
-#         msg = MIMEMultipart()
-#         msg["From"] = sender_email
-#         msg["To"] = email
-#         msg["Subject"] = subject
-#         msg.attach(MIMEText(body, "plain"))
+sender_email = ""
+password = ""
+
+recipients = [
+   # Multiple email
+]
+
+subject = "Application for Potential Opportunities"
+
+body = """Dear Hiring Team,
+
+I hope this message finds you well. I am reaching out to explore potential opportunities.
+Please find my resume attached for your consideration.
+
+Warm regards,
+Ahmed
+"""
+
+
+with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    server.ehlo()
+    server.starttls()
+    server.login(sender_email, password)
+
+    for email in recipients:
+        msg = MIMEMultipart()
+        msg["From"] = sender_email
+        msg["To"] = email
+        msg["Subject"] = subject
+        msg.attach(MIMEText(body, "plain"))
           
         
-#         server.sendmail(sender_email, email, msg.as_string())
-#         print(f"Sent to {email}")
+        server.sendmail(sender_email, email, msg.as_string())
+        print(f"Sent to {email}")
 
-#         time.sleep(10)  # IMPORTANT: delay to avoid spam/blocks
+        time.sleep(10)  # IMPORTANT: delay to avoid spam/blocks
 
-# print("All emails sent individually.")
+print("All emails sent individually.")
 
 
 
@@ -84,15 +81,10 @@ from email.mime.base import MIMEBase
 from email import encoders
 import os
 
-sender_email = "mohd.madani.02@gmail.com"
-password = "wvla enyv kqox fmfs"
+sender_email = "your email"
+password = "Your password"
 
-recipients = [
-    "ahmedqudsi786@gmail.com",
-    "mr.khans00966@gmail.com",
-    "qadriasma3@gmail.com",
-    "mohd.madani.02@gmail.com"
-]
+recipients = [ # write multiple email]
 
 subject = "Application for Potential Opportunities"
 
@@ -105,7 +97,7 @@ Ahmed
 """
 
 # 📎 Path to the file you want to attach
-file_path = "C:\\Users\\Younus\\Downloads\\Madani DXB R.pdf"
+file_path = "# location of file"
 
 with smtplib.SMTP("smtp.gmail.com", 587) as server:
     server.ehlo()
